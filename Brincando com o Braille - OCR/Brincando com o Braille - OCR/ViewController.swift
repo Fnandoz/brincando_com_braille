195891aec54934eaa5782b9d79ded7ebeb222a61
Fernando Gomes <lfgsnando@hotmail.com>
2017-11-06 11:46:14 -0300

    Atualizacao do copyright e lancamento da versão 0.1

---
+++ Brincando com o Braille - OCR/Brincando com o Braille - OCR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brincando com o Braille - OCR
+//
+//  Created by Fernando Gomes on 29/10/2017.
+//  Copyright © 2017 Fernando Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
